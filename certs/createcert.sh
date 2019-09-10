919b0b098c9945bee035543bac8cce6f5f859d8d
tibor.somodi <sotiwin@gmail.com>
2019-09-10 12:14:35 +0200

    fix createcert.sh: remove newlines

--- certs/createcert.sh
+++ certs/createcert.sh
@@ -3,4 +3,5 @@ openssl req -nodes -new -keyout nyomio.local.pem -out nyomio.local.csr -config c
 openssl x509 -req -in nyomio.local.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out nyomio.local.crt -days 730 -sha256 -extfile cert.cfg -extensions v3_req
 NYOMIO_SAN_CERT_BASE64="$(base64 nyomio.local.crt)"
 NYOMIO_SAN_KEY_BASE64="$(base64 nyomio.local.pem)"
-sed "s/NYOMIO_SAN_CERT_BASE64/$NYOMIO_SAN_CERT_BASE64/g" ../install/traefikvalues.template.yml | sed "s/NYOMIO_SAN_KEY_BASE64/$NYOMIO_SAN_KEY_BASE64/g" > ../install/traefikvalues.yml
+sed -e "s/NYOMIO_SAN_CERT_BASE64/${NYOMIO_SAN_CERT_BASE64//$'\n'/}/g" ../install/traefikvalues.template.yml \
+  | sed "s/NYOMIO_SAN_KEY_BASE64/${NYOMIO_SAN_KEY_BASE64//$'\n'/}/g" > ../install/traefikvalues.yml

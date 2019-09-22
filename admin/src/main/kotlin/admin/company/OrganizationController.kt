18c193da3021ba64419c0200bc08b699c637e6ab
Istvan Szoboszlai <istvan.szoboszlai@inepex.com>
2019-09-22 15:54:14 +0200

    Very simple backend for organization handling implemented

--- admin/src/main/kotlin/admin/company/OrganizationController.kt
+++ admin/src/main/kotlin/admin/company/OrganizationController.kt
@@ -2,13 +2,25 @@ package admin.company
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 
 @Controller("/api/v1/admin/organization")
 class OrganizationController
- constructor(private val organizationDbService: OrganizationDbService){
+constructor(private val organizationDbService: OrganizationDbService) {
 
     @Get(uri = "/all")
     fun list() = organizationDbService.listAll()
 
+    @Get(uri = "/{id}")
+    fun getById(id: Long) = organizationDbService.getById(id)
+
+    @Post(uri = "/")
+    fun add(organization: Organization) = organizationDbService.add(organization)
+
+    @Get(uri = "/add-test-data")
+    fun addTestData() = organizationDbService.addTestData()
+
+    @Get(uri = "/create-schema")
+    fun createSchema() = organizationDbService.createTables()
 
 }
--- admin/src/main/kotlin/admin/db/DbAccess.kt
+++ admin/src/main/kotlin/admin/db/DbAccess.kt
@@ -1,9 +1,23 @@
 package admin.db
 
+import io.micronaut.context.annotation.Property
+import org.jetbrains.exposed.sql.Database
 import javax.inject.Singleton
 
 @Singleton
-class DbAccess {
+class DbAccess
+constructor(
+        @Property(name = "db.connection-string")
+        private val dbConnectionString: String,
+        @Property(name = "db.driver")
+        private val dbDriver: String,
+        @Property(name = "db.user")
+        private val dbUser: String,
+        @Property(name = "db.password")
+        private val dbPass: String){
 
+    val db by lazy {
+        Database.connect(dbConnectionString, dbDriver, dbUser, dbPass)
+    }
 }
 

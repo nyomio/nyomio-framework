c3c1ce2789d70c173a892ce91fece5148ebd1f45
tibor.somodi <sotiwin@gmail.com>
2019-11-15 16:04:41 +0100

    updated nyomio-ng-components to 0.1.9, filtering

--- nyom-app/src/app/page/organization/state/organization.service.ts
+++ nyom-app/src/app/page/organization/state/organization.service.ts
@@ -5,14 +5,17 @@ import {Organization} from './organization.model';
 import {OrganizationQuery} from "./organization.query";
 import {handleHttpError} from "../../../common/error/error.util";
 import {EntityEditorService} from "nyomio-ng-components";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Injectable({providedIn: 'root'})
 export class OrganizationService extends EntityEditorService {
 
-  constructor(private organizationStore: OrganizationStore,
+  constructor(router: Router,
+              route: ActivatedRoute,
+              private organizationStore: OrganizationStore,
               private organizationQuery: OrganizationQuery,
               private http: HttpClient) {
-    super(organizationStore, organizationQuery)
+    super(router, route, organizationStore, organizationQuery)
   }
 
   getAt(timestamp: number, filter?: string) {

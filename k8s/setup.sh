69d028e8c201c725fcbba58252ef673eb4b4b960
Istvan Szoboszlai <istvan.szoboszlai@inepex.com>
2019-12-10 17:37:33 +0100

    Updated local cert generation for new kustomization based deploy. Added setup.sh for simple local setup.

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,11 +1,37 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
 
 kubectl create namespace framework-demo
+
 kubectl -n framework-demo create secret generic postgres-user \
-        --from-literal=password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+        --from-literal=password=$(openssl rand -base64 16)
+
 kubectl -n framework-demo create secret generic keycloak-initial-admin \
-        --from-literal=password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+        --from-literal=password=$(openssl rand -base64 16)
+
 kubectl -n framework-demo create secret generic nyom-app \
-        --from-literal=jwt-secret=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+        --from-literal=jwt-secret=$(openssl rand  -base64 64 | tr -d '\n')
+
 kubectl config set-context --current --namespace=framework-demo
+
+
+cd ..
+
+if [[ "$*" == "buildAuth" ]]; then
+  # Build auth microservice
+  printf "\n%s\n" "***** Building and starting auth microservice..."
+  auth/build-docker.sh
+fi
+
+if [[ "$*" == "buildAdmin" ]]; then
+  # Build admin microservice
+  printf "\n%s\n" "***** Building and starting admin microservice..."
+  admin/build-docker.sh
+fi
+
+if [[ "$*" == "buildWeb" ]]; then
+  # Build angular webapp
+  printf "\n%s\n" "***** Building and starting admin microservice..."
+  nyom-app/build-docker.sh
+fi
+
